638fce000d9b4f8be4338232dd7c0e72f3a35584
kowariknando <kowariknando@gmail.com>
2021-03-29 20:12:36 +0200

    This is the first commit

---
+++ TestCase.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_firstTest(self):
+        print("this is the first unit Test case")
+
+
+if __name__ == "__main__":
+    unittest.main()
